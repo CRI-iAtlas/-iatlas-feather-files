ac013ee0f79c1f04ef0401bee1388f57390828a5
andrewelamb <andrewelamb@gmail.com>
2020-06-16 13:10:34 -0700

    datasets added to files

--- R/feather_file_creator-pcawg_build_samples_files.R
+++ R/feather_file_creator-pcawg_build_samples_files.R
@@ -1,33 +1,22 @@
 pcawg_build_samples_files <- function() {
 
-  cat_samples_status <- function(message) {
-    cat(crayon::cyan(paste0(" - ", message)), fill = TRUE)
-  }
+  require(magrittr)
 
   get_samples <- function() {
 
     cat(crayon::magenta(paste0("Get PCAWG samples.")), fill = TRUE)
 
-    cat_samples_status("Get the initial values from Synapse.")
     samples <- iatlas.data::get_pcawg_samples_cached() %>%
       dplyr::select(patient_barcode = sample) %>%
-      dplyr::mutate(name = patient_barcode)
+      dplyr::mutate("name" = patient_barcode, "dataset" = "TCGA") %>%
+      dplyr::arrange(name)
 
     return(samples)
   }
 
-  .GlobalEnv$pcawg_patients <- iatlas.data::synapse_store_feather_file(
+  iatlas.data::synapse_store_feather_file(
     get_samples(),
     "pcawg_samples.feather",
     "syn22125724"
   )
-
-  # Log out of Synapse.
-  iatlas.data::synapse_logout()
-
-  ### Clean up ###
-  # Data
-  rm(pcawg_samples, pos = ".GlobalEnv")
-  cat("Cleaned up.", fill = TRUE)
-  gc()
 }
--- R/feather_file_creator-tcga_build_samples_files.R
+++ R/feather_file_creator-tcga_build_samples_files.R
@@ -1,31 +1,23 @@
 tcga_build_samples_files <- function() {
 
-  iatlas.data::create_global_synapse_connection()
+  require(magrittr)
 
   get_samples <- function() {
 
     cat(crayon::magenta(paste0("Get samples.")), fill = TRUE)
 
     samples <- "syn22128019" %>%
-      .GlobalEnv$synapse$get() %>%
-      purrr::pluck("path") %>%
-      feather::read_feather(.) %>%
+      iatlas.data::synapse_feather_id_to_tbl() %>%
       dplyr::select("name" = "ParticipantBarcode") %>%
-      dplyr::mutate("patient_barcode" = name) %>%
+      dplyr::mutate("patient_barcode" = name, "dataset" = "TCGA") %>%
       dplyr::arrange(name)
 
     return(samples)
   }
 
-  .GlobalEnv$tcga_samples <- iatlas.data::synapse_store_feather_file(
+  iatlas.data::synapse_store_feather_file(
     get_samples(),
     "tcga_samples.feather",
     "syn22125724"
   )
-
-  ### Clean up ###
-  # Data
-  rm(tcga_samples, pos = ".GlobalEnv")
-  cat("Cleaned up.", fill = TRUE)
-  gc()
 }
