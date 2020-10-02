2db8a9a7ace93db22faa2477bf832c1994e1c046
andrewelamb <andrewelamb@gmail.com>
2020-10-02 12:30:33 -0700

    testing changes

--- R/feather_file_creator-tcga_build_genes_to_samples_files.R
+++ R/feather_file_creator-tcga_build_genes_to_samples_files.R
@@ -17,13 +17,17 @@ tcga_build_genes_to_samples_files <- function() {
 
     tcga_samples <- "syn22139885" %>%
       iatlas.data::synapse_feather_id_to_tbl(.) %>%
-      dplyr::pull("name")
+      dplyr::pull("name") %>%
+      sort() %>%
+      .[1:1000]
 
     tcga_aliquots <- "syn21435422" %>%
       synapse_delimited_id_to_tbl() %>%
       dplyr::rename("patient" = 1, "aliqout" = 2) %>%
       tidyr::drop_na() %>%
-      dplyr::pull("aliqout")
+      dplyr::pull("aliqout") %>%
+      sort() %>%
+      .[1:1000]
 
     expression <- "syn22890627" %>%
       synapse_delimited_id_to_tbl() %>%
@@ -39,27 +43,27 @@ tcga_build_genes_to_samples_files <- function() {
       ) %>%
       tidyr::drop_na()
 
-    return(expression)
+    copy_number <- "syn22889333" %>%
+      synapse_delimited_id_to_tbl() %>%
+      dplyr::select(-'Locus ID', -'Cytoband') %>%
+      dplyr::rename_with(~stringr::str_sub(.x, 1, 12)) %>%
+      dplyr::rename("hgnc" = "Gene Symbol") %>%
+      dplyr::inner_join(tcga_hgnc_to_entrez, by = "hgnc") %>%
+      dplyr::select(dplyr::any_of(c("entrez", tcga_samples))) %>%
+      tidyr::pivot_longer(
+        -"entrez", names_to = "sample", values_to = "copy_number"
+      ) %>%
+      tidyr::drop_na()
 
-    # copy_number <- "syn22889333" %>%
-    #   synapse_delimited_id_to_tbl() %>%
-    #   dplyr::select(-'Locus ID', -'Cytoband') %>%
-    #   dplyr::rename_with(~stringr::str_sub(.x, 1, 12)) %>%
-    #   dplyr::rename("hgnc" = "Gene Symbol") %>%
-    #   dplyr::inner_join(tcga_hgnc_to_entrez, by = "hgnc") %>%
-    #   dplyr::select(dplyr::any_of(c("entrez", tcga_samples))) %>%
-    #   tidyr::pivot_longer(
-    #     -"entrez", names_to = "sample", values_to = "copy_number"
-    #   ) %>%
-    #   tidyr::drop_na()
-    #
-    # dplyr::full_join(expression, copy_number, by = c("entrez", "sample"))
+    x <- dplyr::full_join(expression, copy_number, by = c("entrez", "sample"))
   }
 
-  .GlobalEnv$pcawg_genes_to_samples <- iatlas.data::synapse_store_feather_file(
-    get_genes_to_samples(),
+  iatlas.data::synapse_store_feather_file(
+    # get_genes_to_samples(),
+    x,
     "tcga_genes_to_samples.feather",
-    "syn22125645"
+    # "syn22125645",
+    "syn22923960"
   )
 
 
