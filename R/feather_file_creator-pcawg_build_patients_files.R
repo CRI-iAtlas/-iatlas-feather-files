49651a5aa611064378f634f3465784181dd58970
andrewelamb <andrewelamb@gmail.com>
2021-02-11 10:01:14 -0800

    updated pcawg patients to use new gener and age info

--- R/feather_file_creator-pcawg_build_patients_files.R
+++ R/feather_file_creator-pcawg_build_patients_files.R
@@ -1,32 +1,23 @@
 pcawg_build_patients_files <- function() {
+  synapser::synLogin()
+  patient_clinical_data <- "syn24827063" %>%
+    synapser::synGet() %>%
+    purrr::pluck("path") %>%
+    readxl::read_excel(.) %>%
+    dplyr::select(
+      "barcode" = "icgc_donor_id",
+      "gender" = "donor_sex",
+      "age_at_diagnosis" = "donor_age_at_diagnosis"
+    )
 
-  cat_patients_status <- function(message) {
-    cat(crayon::cyan(paste0(" - ", message)), fill = TRUE)
-  }
+  patients <- iatlas.data::get_pcawg_samples_cached() %>%
+    dplyr::select(barcode = sample) %>%
+    dplyr::left_join(patient_clinical_data)
 
-  get_patients <- function() {
 
-    cat(crayon::magenta(paste0("Get PCAWG patients.")), fill = TRUE)
-
-    cat_patients_status("Get the initial values from Synapse.")
-    patients <- iatlas.data::get_pcawg_samples_cached() %>%
-      dplyr::select(barcode = sample)
-
-    return(patients)
-  }
-
-  .GlobalEnv$pcawg_patients <- iatlas.data::synapse_store_feather_file(
-    get_patients(),
+  iatlas.data::synapse_store_feather_file(
+    patients,
     "pcawg_patients.feather",
     "syn22125717"
   )
-
-  # Log out of Synapse.
-  iatlas.data::synapse_logout()
-
-  ### Clean up ###
-  # Data
-  rm(pcawg_patients, pos = ".GlobalEnv")
-  cat("Cleaned up.", fill = TRUE)
-  gc()
 }
