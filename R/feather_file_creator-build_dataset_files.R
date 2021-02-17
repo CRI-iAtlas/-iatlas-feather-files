d8adeb0b7a0e24e3ad6370f6d1ad9ec8c87e9e02
Andrew Lamb <andrewelamb@gmail.com>
2021-02-17 11:42:58 -0800

    add gtex to datasets

--- R/feather_file_creator-build_dataset_files.R
+++ R/feather_file_creator-build_dataset_files.R
@@ -6,6 +6,7 @@ build_dataset_table <- function(){
     dplyr::select("display" = "Dataset") %>%
     dplyr::mutate("type" = "ici") %>%
     dplyr::add_row("display" = c("TCGA", "PCAWG"), "type" = "analysis") %>%
+    dplyr::add_row("display" = c("GTEX"), "type" = "other") %>%
     dplyr::mutate("name" = stringr::str_replace_all(.data$display, " ", "_"))
 
   iatlas.data::synapse_store_feather_file(
