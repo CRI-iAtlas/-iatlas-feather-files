4c71eec4a334ebce690ebbb734a4a36849205343
Andrew Lamb <andrewelamb@gmail.com>
2021-03-25 08:30:22 -0700

    updated

--- R/feather_file_creator-tcga_build_gwas_files.R
+++ R/feather_file_creator-tcga_build_gwas_files.R
@@ -1,6 +1,9 @@
 coloc <- function(){
   require(magrittr)
 
+  heritability <- "syn23651688" %>%
+    synapse_feather_id_to_tbl()
+
   gwas <-
     "syn24298976" %>%
     synapse_feather_id_to_tbl() %>%
@@ -14,7 +17,7 @@ coloc <- function(){
     ) %>%
     dplyr::mutate("dataset" = "TCGA")
 
-  #category module nullable
+  # keys: feature snp dataset
 
   snps <-
     "syn24298976" %>%
@@ -32,48 +35,69 @@ coloc <- function(){
       .data$rsid
     ))
 
-  # snps rsid nullable
-
-
-  genes <- "syn22240716" %>%
-    synapse_feather_id_to_tbl()
-
-  coloc_gtex <- "syn24202047" %>%
-    synapse_feather_id_to_tbl()
-
-  # keys: feature, snp, qtl, gene, splice, tissue
-
-
-  coloc_tcga <- "syn24202048" %>%
-    synapse_feather_id_to_tbl()
-
-  # keys: feature, snp, qtl, gene, splice, c1c2, plotype
-
   coloc <-
     dplyr::bind_rows(
       "syn24202048" %>%
         synapse_feather_id_to_tbl() %>%
-        dplyr::mutate("source" = "TCGA"),
-      "syn24202048" %>%
+        dplyr::mutate("coloc_dataset" = "TCGA"),
+      "syn24202047" %>%
         synapse_feather_id_to_tbl() %>%
-        dplyr::mutate("source" = "GTEX"),
+        dplyr::mutate("coloc_dataset" = "GTEX"),
     ) %>%
-    dplyr::rename("hgnc" = "gene") %>%
-    dplyr::left_join(genes, by = "hgnc")
+    dplyr::select(-c("display", "snp_id", "chr", "bp")) %>%
+    dplyr::rename(
+      "snp" = "snp_col",
+      "qtl_type" = "QTL",
+      "splice_loc" = "splice",
+      "plot_link" = "link_plot",
+      "ecaviar_pp" = "C1C2"
+    ) %>%
+    dplyr::mutate(
+      "dataset" = "TCGA",
+      "splice_loc" = dplyr::if_else(
+        .data$splice_loc == "",
+        NA_character_,
+        .data$splice_loc
+      )
+    )
 
-  missing_genes <- coloc %>%
-    dplyr::filter(is.na(.data$entrez)) %>%
-    dplyr::pull("hgnc") %>%
-    unique()
-
-  coloc <- coloc %>%
-    dplyr::filter(!is.na(.data$entrez))
-
-  rare_vars <- "syn24298988" %>%
-    synapse_feather_id_to_tbl()
+  rv_pathway_associations <- "syn24298988" %>%
+    synapse_feather_id_to_tbl() %>%
+    dplyr::rename(
+      "n_mutants" = "n_mutations",
+      "n_total" = "n"
+    ) %>%
+    dplyr::select(-c("display", "germline_category", "germline_module")) %>%
+    dplyr::mutate("dataset" = "TCGA")
 
 
+  # keys feature pathway dataset
 
+  germline_features <-
+    dplyr::bind_rows(
+      heritability,
+      gwas,
+      coloc,
+      rv_pathway_associations
+    ) %>%
+    dplyr::select(
+      "name" = "feature",
+      "germline_category" = "category",
+      "germline_module" = "module"
+    ) %>%
+    dplyr::distinct() %>%
+    tidyr::drop_na() %>%
+    dplyr::group_by(name) %>%
+    dplyr::arrange(germline_category, germline_module) %>%
+    dplyr::slice(1) %>%
+    dplyr::ungroup()
+
+  # synapse_store_feather_file(
+  #   rv_pathway_associations,
+  #   "tcga_rare_variant_pathway_associations.feather",
+  #   "syn24873865"
+  # )
+  # synapse_store_feather_file(germline_features, "tcga_germline_feature_cols.feather", "syn22125617")
   # synapse_store_feather_file(gwas, "tcga_germline_GWAS.feather", "syn24202036")
   # synapse_store_feather_file(snps, "tcga_germline_GWAS_snps.feather", "syn24202034")
 }
