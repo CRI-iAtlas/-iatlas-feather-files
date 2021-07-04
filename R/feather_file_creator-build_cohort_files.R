a645658e0dab8fa8440983c2534e82aed700fa17
Andrew Lamb <andrewelamb@gmail.com>
2021-07-04 14:23:36 -0700

    redo cohorts

--- R/feather_file_creator-build_cohort_files.R
+++ R/feather_file_creator-build_cohort_files.R
@@ -1,18 +1,18 @@
 build_cohorts_table <- function(){
 
   tbl <- dplyr::tribble(
-    ~name,                  ~dataset, ~tag,             ~clinical,
-    "TCGA",                 "TCGA",   NA,               NA,
-    "TCGA_Immune_Subtype",  "TCGA",   "Immune_Subtype", NA,
-    "TCGA_TCGA_Subtype",    "TCGA",   "TCGA_Subtype",   NA,
-    "TCGA_TCGA_Study",      "TCGA",   "TCGA_Study",     NA,
-    "TCGA_Gender",          "TCGA",   NA,               "Gender",
-    "TCGA_Race",            "TCGA",   NA,               "Race",
-    "TCGA_Ethnicity",       "TCGA",   NA,               "Ethnicity",
-    "PCAWG",                "PCAWG",  NA,               NA,
-    "PCAWG_Immune_Subtype", "PCAWG",  "Immune_Subtype", NA,
-    "PCAWG_PCAWG_Study",    "PCAWG",  "PCAWG_Study",    NA,
-    "PCAWG_Gender",         "PCAWG",  NA,               "Gender"
+    ~name,                  ~dataset, ~tag,
+    "TCGA",                 "TCGA",   NA,
+    "TCGA_Immune_Subtype",  "TCGA",   "Immune_Subtype",
+    "TCGA_TCGA_Subtype",    "TCGA",   "TCGA_Subtype",
+    "TCGA_TCGA_Study",      "TCGA",   "TCGA_Study",
+    "TCGA_Gender",          "TCGA",   "gender",
+    "TCGA_Race",            "TCGA",   "race",
+    "TCGA_Ethnicity",       "TCGA",   "ethnicity",
+    "PCAWG",                "PCAWG",  NA,
+    "PCAWG_Immune_Subtype", "PCAWG",  "Immune_Subtype",
+    "PCAWG_PCAWG_Study",    "PCAWG",  "PCAWG_Study",
+    "PCAWG_Gender",         "PCAWG",  "gender"
   )
 
   iatlas.data::synapse_store_feather_file(
